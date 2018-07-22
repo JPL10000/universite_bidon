7e47ce12e06cccb059796314545a30c8cfdd960c
Unknown <jeanpierreleac@lescahiersdelinnovation.com>
2018-07-22 23:03:49 +0200

    menus2

--- app/sql/local.wp_terms.sql
+++ app/sql/local.wp_terms.sql
@@ -2,4 +2,7 @@
 /*!40014 SET FOREIGN_KEY_CHECKS=0*/;
 /*!40103 SET TIME_ZONE='+00:00' */;
 INSERT INTO `wp_terms` VALUES
-(1,"Uncategorized","uncategorized",0);
+(1,"Uncategorized","uncategorized",0),
+(2,"Mon menu d&#039;entête","mon-menu-dentete",0),
+(3,"Mon menu magique","mon-menu-magique",0),
+(4,"Mon menu super magique","mon-menu-super-magique",0);
--- app/sql/local.wp_usermeta.sql
+++ app/sql/local.wp_usermeta.sql
@@ -17,6 +17,9 @@ INSERT INTO `wp_usermeta` VALUES
 (13,1,"wp_user_level","10"),
 (14,1,"dismissed_wp_pointers","wp496_privacy"),
 (15,1,"show_welcome_panel","1"),
-(16,1,"session_tokens","a:1:{s:64:\"3688d0126502bc6bcea0b9c1f083130703d3104eebf6d81f7ef1ab969e1ead56\";a:4:{s:10:\"expiration\";i:1532186464;s:2:\"ip\";s:10:\"172.17.0.1\";s:2:\"ua\";s:78:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0\";s:5:\"login\";i:1532013664;}}"),
+(16,1,"session_tokens","a:2:{s:64:\"35c2bcebac0075ce9941e0328152d3d7494e06375781cf8913fa027d6bf00b04\";a:4:{s:10:\"expiration\";i:1532359711;s:2:\"ip\";s:10:\"172.17.0.1\";s:2:\"ua\";s:78:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0\";s:5:\"login\";i:1532186911;}s:64:\"3925db045080f380ba207e588019cd4228749d4c0c1c962c9ca2336bcfe64fcd\";a:4:{s:10:\"expiration\";i:1532455827;s:2:\"ip\";s:10:\"172.17.0.1\";s:2:\"ua\";s:78:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0\";s:5:\"login\";i:1532283027;}}"),
 (17,1,"wp_dashboard_quick_press_last_post_id","4"),
-(18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}");
+(18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}"),
+(19,1,"managenav-menuscolumnshidden","a:5:{i:0;s:11:\"link-target\";i:1;s:11:\"css-classes\";i:2;s:3:\"xfn\";i:3;s:11:\"description\";i:4;s:15:\"title-attribute\";}"),
+(20,1,"metaboxhidden_nav-menus","a:1:{i:0;s:12:\"add-post_tag\";}"),
+(21,1,"nav_menu_recently_edited","2");
